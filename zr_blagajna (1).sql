f5b0452d03ab05e2f209fb4de6538b2e085c3407
aonkrut <113457263+aonkrut@users.noreply.github.com>
2024-02-26 11:00:19 +0100

    SIG. KOPIJA
    baza

---
+++ zr_blagajna (1).sql
@@ -0,0 +1,99 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 26, 2024 at 10:57 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `zr_blagajna`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dobavljaci`
+--
+-- Error reading structure for table zr_blagajna.dobavljaci: #1932 - Table &#039;zr_blagajna.dobavljaci&#039; doesn&#039;t exist in engine
+-- Error reading data for table zr_blagajna.dobavljaci: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `zr_blagajna`.`dobavljaci`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `prava`
+--
+-- Error reading structure for table zr_blagajna.prava: #1932 - Table &#039;zr_blagajna.prava&#039; doesn&#039;t exist in engine
+-- Error reading data for table zr_blagajna.prava: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `zr_blagajna`.`prava`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pravo`
+--
+-- Error reading structure for table zr_blagajna.pravo: #1932 - Table &#039;zr_blagajna.pravo&#039; doesn&#039;t exist in engine
+-- Error reading data for table zr_blagajna.pravo: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `zr_blagajna`.`pravo`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `proizvodi`
+--
+-- Error reading structure for table zr_blagajna.proizvodi: #1932 - Table &#039;zr_blagajna.proizvodi&#039; doesn&#039;t exist in engine
+-- Error reading data for table zr_blagajna.proizvodi: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `zr_blagajna`.`proizvodi`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `racun`
+--
+-- Error reading structure for table zr_blagajna.racun: #1932 - Table &#039;zr_blagajna.racun&#039; doesn&#039;t exist in engine
+-- Error reading data for table zr_blagajna.racun: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `zr_blagajna`.`racun`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `status`
+--
+-- Error reading structure for table zr_blagajna.status: #1932 - Table &#039;zr_blagajna.status&#039; doesn&#039;t exist in engine
+-- Error reading data for table zr_blagajna.status: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `zr_blagajna`.`status`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `status_racuna`
+--
+-- Error reading structure for table zr_blagajna.status_racuna: #1932 - Table &#039;zr_blagajna.status_racuna&#039; doesn&#039;t exist in engine
+-- Error reading data for table zr_blagajna.status_racuna: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `zr_blagajna`.`status_racuna`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stavkaracuna`
+--
+-- Error reading structure for table zr_blagajna.stavkaracuna: #1932 - Table &#039;zr_blagajna.stavkaracuna&#039; doesn&#039;t exist in engine
+-- Error reading data for table zr_blagajna.stavkaracuna: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `zr_blagajna`.`stavkaracuna`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `zaposlenici`
+--
+-- Error reading structure for table zr_blagajna.zaposlenici: #1932 - Table &#039;zr_blagajna.zaposlenici&#039; doesn&#039;t exist in engine
+-- Error reading data for table zr_blagajna.zaposlenici: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `zr_blagajna`.`zaposlenici`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
